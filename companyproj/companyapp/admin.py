6b0b5f0807d3d3d48f7fc1b31e66fe9e0f7f31d5
bhargavidarapureddi <116108219+bhargavidarapureddi@users.noreply.github.com>
2025-03-13 14:27:23 +0530

    Delete companyproj directory

--- companyproj/companyapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin 
-from companyapp.models import Company 
- 
- # Register your models here. 
-class CompanyAdmin(admin.ModelAdmin): 
-	list_display=['name','location','ceo'] 
-
-admin.site.register(Company,CompanyAdmin)

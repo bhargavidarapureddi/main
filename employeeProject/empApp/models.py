f93d4f238befcd4a0fd75946f3dab07eb4c2d47a
bhargavidarapureddi <116108219+bhargavidarapureddi@users.noreply.github.com>
2025-03-13 14:26:58 +0530

    Delete employeeProject directory

--- employeeProject/empApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Employee(models.Model):
-    empno=models.IntegerField()
-    ename=models.CharField(max_length=30)
-    esal=models.FloatField()
-    eaddr=models.CharField(max_length=30)
